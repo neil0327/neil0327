6640142e4c4e58d8768ec612e074806f7781ce08
neil0327 <139446628+neil0327@users.noreply.github.com>
2023-07-15 15:35:41 +0800

    Update test1.py
    try again

--- test1.py
+++ test1.py
@@ -19,7 +19,7 @@ def run_snow_effect():
     st.title("Registration QR codes")
     input_i = st.text_input("Input serial numbers (e.g. 42220001,44230010-44230020,45210001)")
     input_val = input_i.replace(" ", "")
-    
+
     if st.button("Generate"):
         with st.spinner("Generating PDF files..."):
             temp1 = input_val.split(',')
