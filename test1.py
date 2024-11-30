08bfc8c2f9d91d932045627f55f26ba53d9ac497
neil0327 <139446628+neil0327@users.noreply.github.com>
2024-11-30 19:45:12 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 from reportlab.pdfgen import canvas
 from PIL import Image
 import qrcode
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfMerger
 import streamlit as st
 import base64
 import os
